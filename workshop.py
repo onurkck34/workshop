61cdff9c38d6abec563d1848a6c5e5e8d88564ad
Onur Koçak <98240992+onurkck34@users.noreply.github.com>
2022-04-03 13:41:33 +0300

    Update workshop.py

--- workshop.py
+++ workshop.py
@@ -1,6 +1,6 @@
 sayi1 = int(input("sayi 1 i giriniz = "))
 sayi2 = int(input("sayi 2 yi giriniz = "))
-sayi3 = int(input("sayi 3 u giriniz = "))
+
 
 if (sayi1>=sayi2) and (sayi1>=sayi3):
     print("En buyuk sayi 1 dir")
